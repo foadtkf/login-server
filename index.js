b6d7a9389b49df8c12274eb26d1e583ab0470fe6
foadtkf <fakhrulif@gmail.com>
2022-06-16 04:15:12 +0600

    data shown from client

--- index.js
+++ index.js
@@ -7,10 +7,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post('/login', async (req, res) => {
+    try {
+      const { email, password } = req.body
+      console.log(email,password)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Listening to port ${port}`)
-})
+  console.log(`Listening to port ${port}`);
+});
